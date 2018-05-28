b3bdb217b62b689076d3f018521978a77952f7ed
Unknown <g1727075@st.t-kougei.ac.jp>
2018-05-28 17:52:20 +0900

    最初のコミット

---
+++ SimpleCar/Assets/GameDirector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start () {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+	}
+	
+	void Update () {
+        float Length = this.flag.transform.position.x -
+            this.car.transform.position.x;
+        this.distance.GetComponent<Text>().text =
+            "ゴールまで" + Length.ToString("F2") + "m";
+	}
+}
